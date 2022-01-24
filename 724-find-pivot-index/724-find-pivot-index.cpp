a7c02cbf5c2f83bd2591843913c7b7d7d3915aa4
sushant2508 <64349437+sushant2508@users.noreply.github.com>
2022-01-24 18:04:20 +0530

    Time: 164 ms (5.07%), Space: 291.9 MB (5.19%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        
+        vector<int> ans(100000);
+        vector<int> rev(100000);
+        int flag= -1;
+        ans[0]= 0;
+        rev[nums.size()]= 0;
+        rev[nums.size()-1]= nums[nums.size()-1]; 
+         if (nums.size() == 0)
+            return -1;
+        else if (nums.size() == 1)
+            return 0;     
+         for(int i=0; i<nums.size(); ++i)
+             {
+               ans[i+1]= ans[i] + nums[i];
+               }
+        for(int i= nums.size(); i>0; --i)
+              {
+               rev[i-1] = rev[i] + nums[i-1]; 
+              }
+        for(int i=0; i<nums.size(); ++i){
+             if(ans[i]==rev[i+1]){
+                 flag = i;
+                 break;
+               }
+               }   
+            return flag;
+        }       
+     
+};    
