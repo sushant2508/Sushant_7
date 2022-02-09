5831cbda835ff786de98b8770c9fc96bf22f83c1
sushant2508 <64349437+sushant2508@users.noreply.github.com>
2022-02-09 12:37:19 +0530

    Time: 25 ms (10.61%), Space: 18.3 MB (5.40%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -11,9 +11,10 @@ public:
         for(int i=1; i<t; ++i){
             maxl[i]=max(height[i],maxl[i-1]);
         }
-        for(int i=1; i<t; ++i){
-            maxr[t-i-1] = max(height[t-i],maxr[t-i]);
+        for(int i=t-2; i>0; --i){
+            maxr[i] = max(height[i+1],maxr[i+1]);
         }
+        
         for(int i=0; i<t-1; ++i){
             if(maxl[i]>height[i+1] && maxr[i+1]>height[i+1]){
                 w= (min(maxl[i],maxr[i+1])-height[i+1]) + w;
