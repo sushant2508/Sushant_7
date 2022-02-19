6dd2a14fa42e0edbe435effc04f073691d3af871
sushant2508 <64349437+sushant2508@users.noreply.github.com>
2022-02-19 18:50:16 +0530

    Time: 104 ms (68.75%), Space: 51.5 MB (47.66%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -4,12 +4,14 @@ public:
         
         unordered_map<int, int> mp;
         for(int i=0; i<nums.size(); ++i){
-            mp[nums[i]]++;  
+            mp[nums[i]]++;
+            if(mp[nums[i]]>1)
+                 return true;
         }
-        for(auto value : mp){
-            if(value.second >1)
-                return true;
-        }
+        // for(auto value : mp){
+        //     if(value.second >1)
+        //         return true;
+        // }
         return false;
         
         
