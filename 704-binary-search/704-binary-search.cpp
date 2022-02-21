5aba2dedee085bba6f358bedc61a843d9f8d7a9c
sushant2508 <64349437+sushant2508@users.noreply.github.com>
2022-02-21 13:33:57 +0530

    Time: 55 ms (44.81%), Space: 27.6 MB (63.96%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -6,15 +6,9 @@ public:
         int r = nums.size()-1;
         while(l<=r){     
             int mid = (l + (r-l)/2); 
-            if(nums[mid] ==target){
-                return mid;
-            }
-            else if(nums[mid]>target){
-                r = mid -1;
-            }
-            else if(nums[mid]<target){
-                l= mid +1;
-            }            
+            if(nums[mid] ==target) return mid;
+            else if(nums[mid]>target) r = mid -1;
+            else if(nums[mid]<target) l= mid +1;
         }
         return -1;              
     }
