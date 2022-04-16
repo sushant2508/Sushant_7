a5425d47b1df6bee22621c9545e6c12db3243a5f
sushant2508 <64349437+sushant2508@users.noreply.github.com>
2022-04-16 16:35:52 +0530

    Time: 15 ms (77.33%), Space: 13.1 MB (82.27%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -6,10 +6,8 @@ public:
         int col = matrix[0].size();
         vector<long long int> rows(row,INT_MAX);
         vector<long long int> cols(col,INT_MAX);
-            for(int i=0; i<row; ++i){
-            
-            for(int j=0; j<col; ++j){
-                
+            for(int i=0; i<row; ++i){     
+            for(int j=0; j<col; ++j){     
                 if(matrix[i][j]==0){
                     rows[i]=0;
                     cols[j]=0;
@@ -18,13 +16,12 @@ public:
         }
         for(int i=0; i<row; ++i){
             for(int j=0; j<col; ++j){
-               if(rows[i]==0){
+               if(rows[i]==0 || cols[j]==0 ){
                    matrix[i][j]=0;
                }
-                if(cols[j]==0){
-                    matrix[i][j]=0;
-                }
-               
+                // if(cols[j]==0){
+                //     matrix[i][j]=0;
+                // }     
             }
         }
         
