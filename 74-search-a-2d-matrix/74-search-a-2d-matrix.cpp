349d9e2447f5806e25832a3e2eea82ea53e4c950
sushant2508 <64349437+sushant2508@users.noreply.github.com>
2022-04-24 16:15:02 +0530

    Time: 6 ms (48.57%), Space: 9.6 MB (53.45%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -5,9 +5,13 @@ public:
         
         
         for(int i=0; i<matrix.size(); ++i){
-            for(int j=0; j<matrix[0].size(); ++j){
+            if(matrix[i][matrix[0].size()-1] >=target){
+                for(int j=0; j<matrix[0].size(); ++j){
                 if(matrix[i][j]==target) return true;
             }
+                
+            }
+            
         }
         return false;
     }
