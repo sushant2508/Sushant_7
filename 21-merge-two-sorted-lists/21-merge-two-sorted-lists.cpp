a30f7a5935a6999862aefba0eb8d7db01dafe486
sushant2508 <64349437+sushant2508@users.noreply.github.com>
2022-07-22 22:49:42 +0530

    Time: 22 ms (8.70%), Space: 14.6 MB (97.79%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -14,6 +14,7 @@ public:
         
        if(list1==NULL) return list2;
         if(list2==NULL) return list1;
+        if(list1==NULL && list2==NULL) return list1;
         
         if(list1->val > list2->val){
             list2->next = mergeTwoLists(list1, list2->next);
